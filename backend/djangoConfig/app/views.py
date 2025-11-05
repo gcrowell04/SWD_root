75fdfd9e897602f5852f4528f95ab227b27f4f11
gcrowell04 <gcrowell04@gmail.com>
2025-11-05 13:10:42 -0600

    Update views.py

--- backend/djangoConfig/app/views.py
+++ backend/djangoConfig/app/views.py
@@ -1,1 +1,7 @@
-TODO
+
+from django.shortcuts import render
+from .models import Service
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'service_list.html', {'services': services})
