e54175d0b9ae5465fded9864e779e6f3e4972025
gcrowell04 <gcrowell04@gmail.com>
2025-11-05 13:09:23 -0600

    Update urls.py

--- backend/djangoConfig/urls.py
+++ backend/djangoConfig/urls.py
@@ -1,1 +1,8 @@
-TODO
+
+from django.urls import path
+from app.views import service_list
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('services/', service_list, name='service_list'),
+]
